8aa5ff1f3d5f618b2e774a0267164648c07589b4
Richard <rich.lindsay@live.co.uk>
2020-05-03 14:06:56 +0100

    Updated build files

---
+++ client/build/precache-manifest.e8549f4999128eb43875f9542574eeba.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "ecb6a04f28d87d379a11",
+    "url": "/covid-hackathon/client/build/static/css/main.96b22e4a.chunk.css"
+  },
+  {
+    "revision": "ecb6a04f28d87d379a11",
+    "url": "/covid-hackathon/client/build/static/js/main.58bdf67c.chunk.js"
+  },
+  {
+    "revision": "e055d577ffcf176fa877",
+    "url": "/covid-hackathon/client/build/static/js/runtime~main.380feabc.js"
+  },
+  {
+    "revision": "4146a5735a9b376ab2e8",
+    "url": "/covid-hackathon/client/build/static/css/2.d068e95b.chunk.css"
+  },
+  {
+    "revision": "4146a5735a9b376ab2e8",
+    "url": "/covid-hackathon/client/build/static/js/2.d481bbde.chunk.js"
+  },
+  {
+    "revision": "16d5a4f4bb99d502d0ea74d5631c0fd1",
+    "url": "/covid-hackathon/client/build/static/media/headerimg.16d5a4f4.png"
+  },
+  {
+    "revision": "21421f34d2ebd3f3790209a6e91799dd",
+    "url": "/covid-hackathon/client/build/static/media/canvas.21421f34.jpeg"
+  },
+  {
+    "revision": "4809883a6930334651b57fbdb1bf6077",
+    "url": "/covid-hackathon/client/build/static/media/face.4809883a.png"
+  },
+  {
+    "revision": "2568a3f50f4bdb6378ce98f20bc521da",
+    "url": "/covid-hackathon/client/build/static/media/ghost.2568a3f5.png"
+  },
+  {
+    "revision": "1ec1becfc50db142f3ab617178a631f1",
+    "url": "/covid-hackathon/client/build/static/media/daycanvas.1ec1becf.jpg"
+  },
+  {
+    "revision": "457f49af0913feee1dcfb48b68daaf62",
+    "url": "/covid-hackathon/client/build/index.html"
+  }
+];
